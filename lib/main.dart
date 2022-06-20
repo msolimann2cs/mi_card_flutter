cb4dd1cf0529e71c81147781b34d08b3f26f91ef
msolimann2cs <msolimann2cs@gmail.com>
2022-06-20 03:52:06 +0200

    Did the layout challenge from the course

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                height: 100,
                 width: 100,
-                color: Colors.white,
-                child: Text('Container 1'),
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
               ),
               Container(
                 width: 100,
                 height: 100,
                 color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                child: Text('Container 3'),
               ),
             ],
           ),
